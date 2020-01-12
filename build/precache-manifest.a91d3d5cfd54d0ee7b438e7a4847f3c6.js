633b23b784008f836ddd56b25ad603f229b51b56
Jordy Shores <jordan.binskin@gmail.com>
2020-01-12 15:00:59 +1100

    added build for server]

---
+++ build/precache-manifest.a91d3d5cfd54d0ee7b438e7a4847f3c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04000a31e48d827acd91e683a29f3a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a28badb7cf6350530c",
+    "url": "/static/css/main.c889d281.chunk.css"
+  },
+  {
+    "revision": "e53e0add31be256c8c49",
+    "url": "/static/js/2.45a46ecb.chunk.js"
+  },
+  {
+    "revision": "22a28badb7cf6350530c",
+    "url": "/static/js/main.e9c108d8.chunk.js"
+  },
+  {
+    "revision": "6b217e8a6e39d19a0ebc",
+    "url": "/static/js/runtime-main.15bbe1b7.js"
+  }
+]);
